20aefcb7c174ca36bb5fc13f44c89ab58272920e
Kirill <ksazaksss@gmail.com>
2022-01-13 20:34:01 +0200

    Initial commit

---
+++ Lesson3.py
@@ -0,0 +1,68 @@
+# # #Lesson3
+# # #if...elif...else
+# #
+# # print(2+2)
+# # print(2/4)
+# #
+# # # Математические операторы  + - * / // % **
+# # # Логические > < >= =< == !=
+# #
+# # print(4!=4)
+#
+# age = 13 #PEP 8 Cisco
+# if  (age < 18): #  двоеточие значит, что условие окончено/ Скобки - выполнить в начале
+# # 4 пробела после условия!
+#     print('child')
+# if  (age > 18):
+#     print('Adult')
+# if True:
+#     print('ALWAYS')
+# if False:
+#     print('Never')
+# if 'Something'
+#     print('Something')
+
+# age = 18
+# if  (age < 18):
+#     print('Child')
+# if  (age > 18):
+#     print('Adult')
+
+
+# age = 18
+# if  (age < 18):
+#     print('Child')
+# else:
+#     print('Adult')
+
+# weight = 150
+# if (weight < 0):
+#     print('Eror')
+# elif (weight <=150):
+#     print('Ok')
+# else:
+#     print('Oversize')
+
+# age = 15
+# if  age < 0:
+#     print('Eror-')
+# elif  (age < 13):
+#     print('Child')
+# elif  (age < 18):
+#     print('Adult')
+# if 'True'
+
+weight = 100
+if type(weight) != type(1):
+    print('No text allowed')
+elif (weight < 0):
+    print('Eror')
+elif (weight <= 20):
+    print('Try to eat o_O')
+elif (weight <=150):
+    print('Ok')
+elif (weight >=1000):
+    print('Lol, eror =)')
+else:
+    print('Oversize')
+
